bd574033bd58a309d48218dd731f9fc40e6e1bcc
Imran Khan <emrankhanniloy@gmail.com>
2026-09-17 16:31:43 +0600

    Restful apis and Pydantic

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from pydantic import BaseModel, Field
+
+class DocumentCreate(BaseModel):
+    title: str = Field(min_length=1, max_length=100)
+    description: str | None = None
+
+class DocumentResponse(BaseModel):
+    id: int
+    title: str
+    description: str | None = None
 
 @app.get("/")
 def home():
@@ -9,8 +19,29 @@ def home():
         "message": "Ask My Document API is running"
     }
 
-@app.get("/documents/{document_id}")
+
+@app.post("/documents")
+def create_document(document: DocumentCreate):
+    return {
+        "title": document.title,
+        "description": document.description
+    }
+
+@app.get("/documents/search")
+def get_documents(q: str, limit: int = 10):
+    return {
+        "query": q,
+        "limit": limit
+    }
+
+@app.get(
+    "/documents/{document_id}",
+    response_model=DocumentResponse
+)
 def get_document(document_id: int):
     return {
-        "document_id": document_id
+        "id": document_id,
+        "title": "FastAPI notes",
+        "description": "FastAPI notes",
     }
+
